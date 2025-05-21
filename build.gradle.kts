7318db09120db9dc2ebc83e57951e750ad411ca5
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2025-05-21 11:58:49 +0200

    fixed env variables (hopefully)

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,8 +76,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/RyzomApps/RyzomRedditBot")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
             }
         }
     }
